64d37682e486fa40fc2ff63ba7923cd9c15d9147
Jeremy B. Frias <jeremyfrias.pro@gmail.com>
2023-08-15 19:11:11 -0400

    Added a function to toggle between front & back...

--- main.js
+++ main.js
@@ -9,12 +9,18 @@ class FlippingCard {
     }
 
     flipCard(){
-        this.isShowingFront = this.isShowingFront ? false : true;
+        this.toggleBool();
         if(this.isShowingFront){
-            this.card.innerText = this.backContent;
-        } else {
             this.card.innerText = this.frontContent;
+            this.card.style.backgroundColor = "rgb(102, 107, 255)";
+        } else {
+            this.card.innerText = this.backContent;
+            this.card.style.backgroundColor = "rgb(68, 72, 172)";
         }
+    }
+
+    toggleBool(){
+        this.isShowingFront = this.isShowingFront ? false : true;
     }
 
 }
